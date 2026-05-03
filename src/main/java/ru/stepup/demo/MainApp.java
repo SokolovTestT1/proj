046f1d0b893f1a48808090c24fd79d8d6a61035c
irinai <petinvasa770@gmail.com>
2026-05-03 14:51:12 +0300

    main

--- src/main/java/ru/stepup/demo/MainApp.java
+++ src/main/java/ru/stepup/demo/MainApp.java
@@ -1,13 +1,14 @@
 package ru.stepup.demo;
 
 public class MainApp {
-    static void main() {
+    public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println(add(0, 2));
         Cat cat = new Cat();
         cat.setName2("Barsik");
         System.out.println(cat.getName2());
     }
+
 
     public static int add(int a, int b) {
         return a + b;
