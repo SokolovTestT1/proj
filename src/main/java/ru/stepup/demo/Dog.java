cc362c1a52d3f979fdb525b326ffa1bcd5273238
irinai <petinvasa770@gmail.com>
2026-05-03 15:00:15 +0300

    constructor + getter + setter

--- src/main/java/ru/stepup/demo/Dog.java
+++ src/main/java/ru/stepup/demo/Dog.java
@@ -4,4 +4,34 @@ public class Dog {
     private String name;
     private int age;
     private float weight;
+
+    public Dog(String name, int age, float weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
